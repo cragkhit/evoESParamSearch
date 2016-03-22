5ebd7f0f6427fe59ed377189dc57a6ad23a05875
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2016-03-22 18:11:09 +0000

    Update to use ProcessBuilder instead of Runtime.exec()
    - Got consistent results.

--- src/es/dfr/paramsearch/MaxOnes.java
+++ src/es/dfr/paramsearch/MaxOnes.java
@@ -1,8 +1,10 @@
 package es.dfr.paramsearch;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 import ec.*;
 import ec.simple.*;
@@ -15,11 +17,6 @@ public class MaxOnes extends Problem implements SimpleProblemForm {
 	// have access to a random number generator
 	// (in the form: state.random[threadnum] )
 	// and to the output facility
-	private String[] norms = { "s", "w", "ws", "p", "ps", "pw", "pws", "k", "ks", "kw", "kws", "kp", "kps", "kpw",
-			"kpws", "j", "js", "jw", "jws", "jp", "jps", "jpw", "jpws", "jk", "jks", "jkw", "jkws", "jkp", "jkps",
-			"jkpw", "jkpws", "d", "ds", "dw", "dws", "dp", "dps", "dpw", "dpws", "dk", "dks", "dkw", "dkws", "dkp",
-			"dkps", "dkpw", "dkpws", "dj", "djs", "djw", "djws", "djp", "djps", "djpw", "djpws", "djk", "djks", "djkw",
-			"djkws", "djkp", "djkps", "djkpw", "djkpws" };
 	private String[] basicModelArr = { "be", "d", "g", "if", "in", "ine", "p" };
 	private String[] afterEffectArr = { "no", "b", "l" };
 	private String[] dfrNormalizationArr = { "no", "h1", "h2", "h3", "z" };
@@ -31,45 +28,58 @@ public class MaxOnes extends Problem implements SimpleProblemForm {
 		if (!(ind instanceof IntegerVectorIndividual))
 			state.output.fatal("Whoa!  It's not an IntegerVectorIndividual!!!", null);
 		IntegerVectorIndividual ind2 = (IntegerVectorIndividual) ind;
-		/*
-		 * int sum = 0; for (int x = 0; x < ind2.genome.length; x++) { //
-		 * state.output.println(x + ": " + ind2.genome[x]); sum +=
-		 * ind2.genome[x]; }
-		 */
 		/***
 		 * Call ES here
 		 */
 		double fitness = 0.0;
-		int sizeOfN = ind2.genome[4] + 1;
-		String normMode = norms[ind2.genome[3]];
-		String basicModel = basicModelArr[ind2.genome[0]];
-		String afterEffect = afterEffectArr[ind2.genome[1]];
-		String dfrNormalization = dfrNormalizationArr[ind2.genome[2]];
-		state.output.print(sizeOfN + "," + normMode + "," + basicModel + "," + afterEffect + "," + dfrNormalization,
-				Log.D_STDOUT);
+		
+		String normMode = "";
+		if (ind2.genome[0]==1) normMode += "d"; 
+		if (ind2.genome[1]==1) normMode += "j";
+		if (ind2.genome[2]==1) normMode += "k";
+		if (ind2.genome[3]==1) normMode += "p";
+		if (ind2.genome[4]==1) normMode += "s";
+		if (ind2.genome[5]==1) normMode += "w";
+		
+		String basicModel = basicModelArr[ind2.genome[6]];
+		String afterEffect = afterEffectArr[ind2.genome[7]];
+		String dfrNormalization = dfrNormalizationArr[ind2.genome[8]];
+		int sizeOfN = ind2.genome[9] + 1;
+		
+		System.out.print("[" + ind2.genome[0] + "," + ind2.genome[1] + "," 
+				+ ind2.genome[2] + "," + ind2.genome[3] + "," + ind2.genome[4] + "," 
+				+ ind2.genome[5] + "|" + ind2.genome[6] + "|" + ind2.genome[7] + "|" 
+				+ ind2.genome[8]  + "|" + ind2.genome[9] + "] = ");
+		System.out.println(sizeOfN + "," + normMode + "," + basicModel + "," + afterEffect + "," + dfrNormalization);
 
 		String output = "";
-		Process p;
+//		Process p;
 		try {
-            String command = "/home/cragkhit/es_exp/scripts/eval_dfr.sh tests_extracted/ eval_dfr " + sizeOfN
-                            + " " + normMode + " eval_dfr/ " + basicModel + " " + afterEffect + " " + dfrNormalization;
-            // System.out.println(command);
-			p = Runtime.getRuntime().exec(command);
-			p.waitFor();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				output += line;
-			}
-			System.out.println(output);
+//			p = Runtime.getRuntime()
+//					.exec("/Users/Chaiyong/Documents/es_exp/scripts/eval_dfr.sh /Users/Chaiyong/Documents/es_exp/tests_extracted/ eval_dfr " + sizeOfN
+//							+ " " + normMode + " /Users/Chaiyong/Documents/es_exp/eval_dfr/ " + basicModel + " " + afterEffect + " " + dfrNormalization);
+//			p.waitFor();
 			
-			fitness = Double.parseDouble(output.trim());
+			ProcessBuilder pb = new ProcessBuilder("./scripts/eval_dfr.sh", "tests_extracted/", "eval_dfr",
+					String.valueOf(sizeOfN), normMode, "eval_dfr/", basicModel, afterEffect, dfrNormalization);
+			pb.directory(new File("/Users/Chaiyong/Documents/es_exp/"));
+			Process p = pb.start();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+				builder.append(System.getProperty("line.separator"));
+			}
+			String result = builder.toString();
+			
+			System.out.println(result);
+			// parse the result to get fitness value
+			fitness = Double.parseDouble(result.trim());
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		} 
 
 		if (!(ind2.fitness instanceof SimpleFitness))
 			state.output.fatal("Whoa!  It's not a SimpleFitness!!!", null);
