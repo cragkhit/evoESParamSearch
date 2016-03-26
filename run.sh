b90b067afffbc17c9c0b6422473ca6357b649ecc
Chaiyong R <cragkhit@gmail.com>
2016-03-26 13:06:43 +0000

    Updated results of BM25, DFR

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # delete all dfr_* indexes
-curl -XDELETE localhost:9200/dfr_*
-javac -cp "./lib/ecj.23.jar" src/es/dfr/paramsearch/MaxOnes.java 
-mv src/es/dfr/paramsearch/MaxOnes.class bin/es/dfr/paramsearch/MaxOnes.class 
-/usr/bin/time -o dfr.time java -cp "./lib/ecj.23.jar:./bin" ec.Evolve -file dfr.params -p breed.elite.0=2
+curl -XDELETE localhost:9200/bm25_*
+javac -cp "./lib/ecj.23.jar" src/es/dfr/paramsearch/BM25.java 
+mv src/es/dfr/paramsearch/BM25.class bin/es/dfr/paramsearch/BM25.class 
+/usr/bin/time -o bm25.time java -cp "./lib/ecj.23.jar:./bin" ec.Evolve -file bm25.params 
